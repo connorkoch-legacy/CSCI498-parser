ad646cb4dcaa8f6342fbd787371327d65de8d19e
Luke H <luop90@gmx.com>
2020-02-23 16:40:12 -0700

    Added a small debug

--- Java/CFGParse/src/Main.java
+++ Java/CFGParse/src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
 	public static void main(String[] args) throws Exception {
+		// IMPORTANT: the filename for the CFG is taken as a command-line argument
 		CFG grammar = new CFG(args[0]);
 		System.out.println(grammar);
 
