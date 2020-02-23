247f2627b6b2ff2c43c1787e07427f52bfd45fe4
Luke H <luop90@gmx.com>
2020-02-23 16:08:02 -0700

    Added typical overrides

--- Java/CFGParse/src/ProductionRule.java
+++ Java/CFGParse/src/ProductionRule.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Set;
 
 public class ProductionRule {
@@ -36,5 +37,29 @@ public class ProductionRule {
         }
 
         return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("-> ");
+
+        for (AlphabetCharacter c : rhs) {
+            result.append(c).append(" ");
+        }
+
+        return result.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return this.toString().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductionRule that = (ProductionRule) o;
+        return Objects.equals(rhs, that.rhs);
     }
 }
