ad01ac408796000c8c768f8a5de3710320aff46b
Luke H <luop90@gmx.com>
2020-02-23 16:03:39 -0700

    Added some helper methods to ProductionRule

--- Java/CFGParse/src/ProductionRule.java
+++ Java/CFGParse/src/ProductionRule.java
@@ -15,4 +15,26 @@ public class ProductionRule {
     public void addCharacterToRHS(AlphabetCharacter c) {
         rhs.add(c);
     }
+
+    /**
+     * Only a lambda production if it's ONLY L -> lambda
+     * @return
+     */
+    public boolean isLambdaProduction() {
+        return rhs.size() == 1 && rhs.get(0).isLambda();
+    }
+
+    /**
+     * Returns true if *anything* on the RHS is not a nonterminal
+     * @return
+     */
+    public boolean containsTerminal() {
+        for (AlphabetCharacter c : rhs) {
+            if (!c.isNonTerminal()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
